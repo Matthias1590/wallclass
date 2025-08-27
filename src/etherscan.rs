d134cfd269679d85c1442a3dfba0e791694ea78a
Ubuntu <ubuntu@vps-ba18050f.vps.ovh.net>
2025-08-27 13:59:37 +0000

    add rust code

--- src/etherscan.rs
+++ src/etherscan.rs
@@ -1,9 +1,23 @@
-struct Client {
+pub struct Client {
     api_key: String
 }
 
 impl Client {
     pub fn new(api_key: String) -> Self {
         Self { api_key }
+    }
+
+    fn get_url(&self, module: &str, action: &str, address: &str) -> String {
+        format!(
+            "https://api.etherscan.io/api?module={}&action={}&address={}&sort=desc&apikey={}",
+            module, action, address, self.api_key
+        )
+    }
+
+    pub async fn get_transactions(&self, address: &str) -> anyhow::Result<serde_json::Value> {
+        let url = self.get_url("account", "txlist", address);
+        let resp = reqwest::get(&url).await?;
+        let value: serde_json::Value = serde_json::from_str(&resp.text().await?).or(Err(anyhow::anyhow!("Failed to parse JSON")))?;
+        Ok(value)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,28 @@
-mod wallet_classifier;
 mod etherscan;
-
-use ethers::prelude::*;
-use crate::wallet_classifier::WalletClassifier;
-use crate::etherscan;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let etherscan_client = etherscan::Client::new("SMWQ1AEZWM7MEPQ4GYW6WXBVWGCQM6MPII");
+    let etherscan_client = etherscan::Client::new("SMWQ1AEZWM7MEPQ4GYW6WXBVWGCQM6MPII".to_owned());
 
     // let provider = Provider::<Http>::try_from("https://mainnet.infura.io/v3/ae660ba403ee422a80f1855e385ec11c")?;
     // let wallet_classifier = WalletClassifier::new(provider);
 
     let address = "0xf60c2ea62edbfe808163751dd0d8693dcb30019c";
+    // let address = "0x1c237f917f6b2e9b911007ff00613bb0e8647369";
+
+    let transactions = &etherscan_client.get_transactions(address).await?["result"];
+    let arr = transactions.as_array().unwrap();
+
+    // let transactions_arr = transactions;
+    // println!("Transactions: {:?}", transactions_arr.len());
+    // println!("Object? {}", transactions.is_object());
 
     // let class = wallet_classifier.classify(address).await?;
 
     // println!("{:?}", class);
 
-    let bal = etherscan::account::get_balance(Config::new("account".into(), "balance".into(), address.into(), "latest".into(), "SMWQ1AEZWM7MEPQ4GYW6WXBVWGCQM6MPII".into())).await?;
-    println!("Balance: {}", bal);
+    // let bal = etherscan::account::get_balance(Config::new("account".into(), "balance".into(), address.into(), "latest".into(), "SMWQ1AEZWM7MEPQ4GYW6WXBVWGCQM6MPII".into())).await?;
+    // println!("Balance: {}", bal);
 
     Ok(())
 }
